657060024b8202aa8d922d7b34c106040f4aadb6
Samuel Jones <samuel.jones1@ontariotechu.net>
2021-03-28 14:17:32 -0400

    Showing broken folder

---
+++ broken/src/battleship/Battleship.java
@@ -0,0 +1,10 @@
+package battleship;
+
+public class Battleship extends Ship {
+    public static final int battleshipSize = 4;
+
+    public Battleship(int size){
+        super(size);
+    }
+
+}
---
+++ broken/src/battleship/Destroyer.java
@@ -0,0 +1,8 @@
+package battleship;
+
+public class Destroyer extends Ship{
+    public static final int destroyerSize = 3;
+    public Destroyer(int size){
+        super(size);
+    }
+}
