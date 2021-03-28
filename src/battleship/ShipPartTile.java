ac2faf2a9a9ced3fee3d94c009aecea4d642e1b4
Samuel Jones <samuel.jones1@ontariotechu.net>
2021-03-28 14:21:48 -0400

    ignore the /src folder idk why its there it contains old stuff

--- src/battleship/ShipPartTile.java
+++
@@ -1,9 +0,0 @@
-package battleship;
-
-public class ShipPartTile extends BoardTile{
-    private ShipPart part;
-    public ShipPartTile(char symbol, Coordinates coordinates, ShipPart part){
-        super(symbol, coordinates);
-        this.part = part;
-    }
-}
